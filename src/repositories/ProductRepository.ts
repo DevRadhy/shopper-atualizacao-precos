42b00e68bdc526a5426104cf156eb01ea7ccbf1f
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-09-06 20:45:46 -0300

    feat: implement product repository

--- src/repositories/ProductRepository.ts
+++ src/repositories/ProductRepository.ts
@@ -2,6 +2,6 @@ import Product from "../entities/Product";
 
 export abstract class ProductRepository {
   abstract find(): Promise<Product[]>;
-  abstract findOne(id: number): Promise<Product>;
+  abstract findOne(id: number): Promise<Product | null>;
   abstract update(id: number, product: Product): Promise<Product>;
 }
