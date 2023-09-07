25a35a30da4fb884a1da151f971ec040f9c79a90
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2023-09-07 20:57:34 -0300

    feat: add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,9 +2,11 @@ import "express-async-errors";
 import express, { NextFunction, Request, Response } from 'express';
 import { routes } from './routes';
 import { AppError } from "./errors/AppError";
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(routes);
 
 app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
